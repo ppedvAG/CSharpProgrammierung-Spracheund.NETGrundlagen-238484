255dd7b4cff89f789448f14e150d88ce9b0eb58c
ppedvLukasK <lukask@ppedv.de>
2024-06-11 15:03:56 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,18 @@
+﻿namespace M008;
+
+public class AccessModifier
+{
+	public string Name { get; set; } //Überall sichtbar, auch außerhalb vom Projekt
+
+	internal int Alter { get; set; } //Überall sichtbar, aber nur innerhalb vom Projekt
+
+	private string Adresse { get; set; } //Nur innerhalb der Klasse sichtbar
+
+	/////////////////////////////////////////////////////////////////
+	
+	protected int Gehalt { get; set; } //Nur innerhalb der Klasse sichtbar (private), ABER auch in Unterklassen (auch außerhalb)
+
+	protected private string Haarfarbe { get; set; } //Nur innerhalb der Klasse sichtbar (private), aber auch in Unterklassen NUR innerhalb des Projekts
+
+	protected internal string Job { get; set; } //Überall sichtbar, aber nur innerhalb vom Projekt (internal), UND auch in Unterklassen außerhalb
+}
---
+++ M008/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace M008;
+
+/// <summary>
+/// Vererbung
+/// Ermöglicht, generelle Oberklassen zu entwickeln, welche Standardaspekte der Unterklassen vorgeben
+/// Die Properties und Funktionen der Oberklasse werden nach unten vererbt
+/// -> Die Unterklassen haben auch diese Properties & Funktionen
+/// 
+/// Beispiel: Stream
+/// - Stream gibt Schreib-/Leseaktionen vor
+/// - FileStream, MemoryStream, ... spezifizieren dieses Konzept
+/// </summary>
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Max", 23);
+		m.Name = "Max"; //Name wurde von Lebewesen vererbt
+		m.Bewegen(10); //Bewegen wurde von Lebewesen vererbt
+
+		Hund h = new Hund("Bello");
+		//Hund erbt auch Name und Bewegen, aber nicht Sprechen
+	}
+}
+
+public class Lebewesen
+{
+	public string Name { get; set; }
+
+	/// <summary>
+	/// Virtual & Override
+	/// Ermöglicht, spezifische Methoden (Methoden mit virtual) in den Unterklassen zu verändern
+	/// Das Lebewesen -> Der Mensch, Der Hund
+	/// </summary>
+	public virtual void Bewegen(int distanz)
+	{
+        Console.WriteLine($"Das Lebewesen {Name} bewegt sich um {distanz}m.");
+    }
+
+	public Lebewesen(string name)
+	{
+		Name = name;
+	}
+}
+
+/// <summary>
+/// Mit : nach dem Klassennamen kann eine Oberklasse definiert werden
+/// "Mensch erbt von Lebewesen"
+/// "Mensch ist ein Lebewesen"
+/// </summary>
+public class Mensch : Lebewesen
+{
+	public int Alter { get; set; }
+
+	/// <summary>
+	/// Strg + . -> Generate Constructor
+	/// 
+	/// Extra Feld hinten anhängen um dieses beim Konstruktor hinzuzufügen
+	/// </summary>
+	public Mensch(string name, int alter) : base(name)
+	{
+		Alter = alter;
+	}
+
+	public void Sprechen(string text)
+	{
+		//Name von Lebewesen empfangen
+        Console.WriteLine($"{Name} sagt: {text}");
+    }
+
+	/// <summary>
+	/// override + Abstand -> Methode auswählen und generieren lassen
+	/// Spezifische Implementation der Methode für Mensch
+	/// </summary>
+	public override void Bewegen(int distanz)
+	{
+		//base: Selbige Verwendung wie this, aber im Vererbungskontext (zwischen Unterklasse und Oberklasse)
+		//base.Bewegen(distanz): Führe den Code aus der Oberklasse aus
+		Console.WriteLine($"Der Mensch {Name} ({Alter}) bewegt sich um {distanz}m.");
+    }
+}
+
+public class Hund : Lebewesen
+{
+	public Hund(string name) : base(name)
+	{
+	}
+
+	public override void Bewegen(int distanz)
+	{
+		Console.WriteLine($"Der Hund {Name} bewegt sich um {distanz}m.");
+	}
+}
